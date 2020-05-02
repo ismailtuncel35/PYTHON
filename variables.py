76921898bef253b808a836f2f816371931244dcc
İSMAİL TUNCEL <59068694+ismailtuncel35@users.noreply.github.com>
2020-05-02 23:10:02 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,32 @@
+maasALİ=5000
+maasAHMET=4000
+vergi=0.27
+
+print(maasALİ-(maasALİ*vergi))
+print(maasAHMET-(maasAHMET*vergi))
+#değişken tanımlama kuralları
+
+#rakam ile başlayamaz
+number1 = 10
+print(number1)
+#büyük küçük harf duyarlılığı
+
+age=20
+AGE=30
+
+#TÜRKÇE KARAKTER KULLANMAYALIM
+
+x=1
+y=2.3
+name="çınar"
+isStudent=True
+
+#x,y,name,isStudent=(1,2.3,"çınar",True)
+
+a=10
+b=20
+print(a+b) #30
+
+firstname = "tuncel"
+lastname = " ismail"
+print(firstname + lastname)
